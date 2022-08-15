3e17086066e7419a1477536271de65ae66232751
Holden Grissett <holdeng96@gmail.com>
2022-08-15 15:56:13 -0700

    remove Option<> from structs
    Signed-off-by: Holden Grissett <holdeng96@gmail.com>

--- src/find_irreducible_error_event.rs
+++ src/find_irreducible_error_event.rs
@@ -1,6 +1,6 @@
-use std::path::Path;
 
-use std::convert::TryInto;
+use num_bigint::BigUint;
+
 use crate::trellis::trellis::Trellis;
 
 struct VecVisitable<T> {
@@ -14,29 +14,31 @@ struct ColumnType {
 
 pub fn find_irreducible_error_event(
     v: u16, numerators: Vec<u16>, denominator: u16, max_search_distance: usize, trellis: Trellis
-) -> Vec<Vec<Option<u64>>> {
+) -> Vec<Vec<BigUint>> {
     
-    let path: String = "./Simulation_results/".to_owned();
+    let _path: String = "./Simulation_results/".to_owned();
     let k = numerators.len();
 
-    let mut num_string: String = "".to_owned();
+    // let mut num_string: String = "".to_owned();
 
-    for i in 0..k {
-        num_string.push_str(&(numerators[i].to_string() + "_"));
-    }
+    // for i in 0..k {
+    //     num_string.push_str(&(numerators[i].to_string() + "_"));
+    // }
 
-    let mut file_name: String = "Trellis_v_".to_owned();
-    file_name.push_str(&(v.to_string().to_owned() + "_num_" + &num_string + "den_" + &denominator.to_string()));
+    // let mut file_name: String = "Trellis_v_".to_owned();
+    // file_name.push_str(&(v.to_string() + "_num_" + &num_string + "den_" + &denominator.to_string()));
 
     // if !Path::new(&(path + &file_name)).exists() {
     //     println!("no file {}!", file_name);
     //     return;
     // }
 
+    // Need to either use multiple u64s or use a matrix algebra library to finish this now... Since termination
+    // Solution: BigInt
+
     const MAX_ITERATION: usize = 200;
-    let mut zero_state: Vec<Vec<Vec<Option<u64>>>> = vec![];
-    //let column: VecVisitable<VecVisitable<VecVisitable<VecVisitable<Option<u16>>>>> = vec![vec![vec![vec![]; max_search_distance]; trellis.numStates]; 2];
-    let mut column: Vec<Vec<Vec<Vec<Option<u64>>>>> = vec![vec![]; 2];
+    let mut zero_state: Vec<Vec<Vec<BigUint>>> = vec![];
+    let mut column: Vec<Vec<Vec<Vec<BigUint>>>> = vec![vec![]; 2];
 
     //let mut column: ColumnType = ColumnType.new();
 
@@ -50,24 +52,24 @@ pub fn find_irreducible_error_event(
         if i == 0 {
             for input in 1..trellis.numInputSymbols {
                 let next_state = u16::from(*trellis.nextStates.index(0, input)) as usize;
-                if column[index][next_state].len() == 0 {
+                if column[index][next_state].is_empty() {
                     column[index][next_state] = vec![vec![]; max_search_distance];
                 }
                 let weight = trellis.outputs.index(0, input).count_ones();
                 // if input == 2 {
                 //     println!("{} {}", trellis.outputs.index(0, input), weight);
                 // }
-                column[index][next_state as usize][(weight - 1) as usize].push(Some(input as u64));
+                column[index][next_state as usize][(weight - 1) as usize].push(BigUint::from(input));
             }
         } else {
             for current_state in 1..trellis.numStates {
-                if column[prev_index][current_state].len() != 0 {
+                if !column[prev_index][current_state].is_empty() {
                     for distance in 0..max_search_distance {
-                        if column[prev_index][current_state][distance].len() != 0 {
+                        if !column[prev_index][current_state][distance].is_empty() {
                             for input in 0..trellis.numInputSymbols {
 
                                 let next_state = usize::from(*trellis.nextStates.index(current_state, input));
-                                if column[index][next_state].len() == 0 {
+                                if column[index][next_state].is_empty() {
                                     column[index][next_state] = vec![vec![]; max_search_distance];
                                 }
 
@@ -77,14 +79,8 @@ pub fn find_irreducible_error_event(
                                 
                                 if distance + weight < max_search_distance {
                                     let tmp = column[prev_index][current_state][distance].clone();
-                                    let new_path: Vec<Option<u64>> = tmp.iter().map(
-                                        |x|
-                                        match *x {
-                                            Some(y) => {
-                                                Some((y << k) | input as u64) // may want to check for overflow here 😬
-                                            },
-                                            None => None,
-                                        }
+                                    let new_path: Vec<BigUint> = tmp.iter().map(
+                                        |x| (x << k) | BigUint::from(input)
                                     ).collect();
                                     // if distance + weight == 2 && i == 2 && next_state == 0 {
                                     //     println!("{}", ((2 << k) | input as u64));
@@ -101,34 +97,37 @@ pub fn find_irreducible_error_event(
         zero_state.push(column[index][0].clone());
     }
 
-    //println!("{:?}", &zero_state[2]);
-    let length = zero_state.len();
+    println!("{:?}", zero_state);
 
-    let mut error_events: Vec<Vec<Option<u64>>> = vec![vec![]; max_search_distance];
+    let _length = zero_state.len();
+
+    let mut error_events: Vec<Vec<BigUint>> = vec![vec![]; max_search_distance];
     
     for i in 0..MAX_ITERATION {
-        if zero_state[i].len() != 0 {
+        if !zero_state[i].is_empty() {
             for distance in 0..max_search_distance {
-                if zero_state[i][distance].len() != 0 {
-                    if error_events[distance].len() == 0 {
+                if !zero_state[i][distance].is_empty() {
+                    if error_events[distance].is_empty() {
                         error_events[distance] = zero_state[i][distance].clone();
                     } else {
-                        let length: usize =  64 - error_events[distance][0].unwrap().leading_zeros() as usize; // find number of 0s;
-                        let new_path: Vec<Option<u64>> = error_events[distance].iter().map(
-                            |x|
-                            match *x {
-                                Some(y) => {
-                                    Some(y << (k * i - length)) // may want to check for overflow here 😬
-                                },
-                                None => None,
-                            }
+                        let length = get_length(&error_events[distance][0]);
+                        error_events[distance] = error_events[distance].iter().map(
+                            |x| x << (k * (i + 1) - length)
                         ).collect();
+                        error_events[distance].append(&mut zero_state[i][distance].clone());
                     }
                 }
             }
         }
     }
 
-
     error_events
+}
+
+fn get_length(num: &BigUint) -> usize {
+    let mut bits = 0;
+    let bytes = num.to_bytes_be();
+    bits = 8 - bytes[0].leading_zeros() + 8 * (bytes.len() as u32 - 1);
+
+    bits as usize
 }
